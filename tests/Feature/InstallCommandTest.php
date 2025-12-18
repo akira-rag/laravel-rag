9c4edbcc596f97eff74780a00350d6c415e5b130
kid <kidiatoliny@akira-io.com>
2025-12-18 03:44:56 +0100

    test: enable dry run for non-interactive install command in tests

--- tests/Feature/InstallCommandTest.php
+++ tests/Feature/InstallCommandTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use function Pest\Laravel\artisan;
 
 it('runs the install command non-interactively', function (): void {
+    putenv('RAG_INSTALL_DRY_RUN=1');
     artisan('rag:install --force')
         ->expectsOutputToContain('Installer')
         ->expectsOutputToContain('Install complete.')
